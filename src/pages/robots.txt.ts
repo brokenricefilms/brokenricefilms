d1347d350075b7fb0c535c4b1f267e9b1147ca17
Thuan Pham <licitfree@gmail.com>
2023-12-10 12:00:06 +0700

    fix robots stuff

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,12 +2,6 @@ import type { APIRoute } from "astro";
 import { SITE } from "@config";
 
 const robots = `
-User-agent: Googlebot
-Disallow: /nogooglebot/
-
-User-agent: *
-Allow: /
-
 Sitemap: ${new URL("sitemap-index.xml", SITE.website).href}
 `.trim();
 
