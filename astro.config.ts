94f46db8acd2d1d089a4fbb90263503a7611af60
Thuan Pham <licitfree@gmail.com>
2023-12-13 16:43:54 -0800

    feat: inbox link

--- astro.config.ts
+++ astro.config.ts
@@ -38,25 +38,11 @@ export default defineConfig({
   },
   scopedStyleStrategy: "where",
   redirects: {
-    '/resume': {
-      status: 302,
-      destination: 'https://docs.google.com/document/d/1T8sRgGYb5Pzj45lTta8Er-9lUb8XgGxDy38xXKAK4m8'
-    },
-    '/portfolio': {
-      status: 302,
-      destination: 'https://youtube.com/@licitfree/'
-    },
-    '/photos': {
-      status: 302,
-      destination: 'https://twitter.com/licitfree/media'
-    },
-    '/music': {
-      status: 302,
-      destination: 'https://www.youtube.com/playlist?list=PLxkEHjd_ca6f6gyg6plFPajUcPlS5_qcr'
-    },
-    '/meet': {
-      status: 302,
-      destination: 'https://calendar.app.google/phJ3he2qu9t9LAKL7'
-    },
+    '/resume': 'https://docs.google.com/document/d/1T8sRgGYb5Pzj45lTta8Er-9lUb8XgGxDy38xXKAK4m8',
+    '/portfolio': 'https://youtube.com/@licitfree/',
+    '/photos': 'https://twitter.com/licitfree/media',
+    '/music': 'https://www.youtube.com/playlist?list=PLxkEHjd_ca6f6gyg6plFPajUcPlS5_qcr',
+    '/meet': 'https://calendar.app.google/phJ3he2qu9t9LAKL7',
+    '/inbox': 'https://www.figma.com/file/Eh8ircd2aLDjE37eEWasj8/inbox?type=whiteboard&node-id=0-1&t=1YZUDygatvP68nrz-0'
   }
 });
