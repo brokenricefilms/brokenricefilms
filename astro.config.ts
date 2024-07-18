35be217245c273dfcdeb894ff407831560416a66
Broken Rice Films <brokenricefilms@gmail.com>
2024-07-18 13:18:33 +0700

    auto commit

--- astro.config.ts
+++ astro.config.ts
@@ -5,11 +5,16 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import vercel from "@astrojs/vercel/serverless";
 
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   site: SITE.website,
   prefetch: {
     prefetchAll: true,
